5a741c9f6f1c0ce708baf78804125a9e78ff02bd
jayPark8282 <jaypark8282@gmail.com>
2024-03-02 00:15:29 +0900

    add: 2차 기본 구성 swagger추가

--- base-module/src/main/java/com/jaypark8282/base/BaseApplication.java
+++ base-module/src/main/java/com/jaypark8282/base/BaseApplication.java
@@ -3,7 +3,9 @@ package com.jaypark8282.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jaypark8282.base", "com.jaypark8282.core"})
 public class BaseApplication {
---
+++ base-module/src/main/java/com/jaypark8282/base/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.jaypark8282.base.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("base 프로젝트 API")
+                        .description("base 기능을 제공합니다.")
+                        .version("1.0.0"));
+    }
+}
