11ae785982bba85c708f28d546e7f3a1f6b96e12
jayPark88 <jaypark8282@gmail.com>
2024-06-08 22:16:43 +0900

    modify: LogInterceptor 로그 내용 보강, SampleGetRequestDto Setter 추가, 로그 설정 수정

--- core-module/src/main/java/com/jaypark8282/core/interceptor/LogInterceptor.java
+++ core-module/src/main/java/com/jaypark8282/core/interceptor/LogInterceptor.java
@@ -12,32 +12,45 @@ import java.util.UUID;
 public class LogInterceptor implements HandlerInterceptor {
 
     public static final String LOG_ID = "logId";
+    public static final String START_TIME = "startTime";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
-
+        String queryString = request.getQueryString();
         String uuid = UUID.randomUUID().toString();
-        request.setAttribute(LOG_ID, uuid);
+        long startTime = System.currentTimeMillis();
 
-        log.info("REQUEST  [{}][{}][{}]", uuid, requestURI, handler);
-        return true; //false 진행X
+        request.setAttribute(LOG_ID, uuid);
+        request.setAttribute(START_TIME, startTime);
+
+        log.info("REQUEST  [{}][{}][{}][{}]", uuid, requestURI, queryString, handler);
+
+        // 요청 헤더 로깅
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> log.info("Header [{}]: {}", headerName, request.getHeader(headerName)));
+
+        return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse
-            response, Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         log.info("postHandle [{}]", modelAndView);
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse
-            response, Object handler, Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         String requestURI = request.getRequestURI();
         String logId = (String) request.getAttribute(LOG_ID);
-        log.info("RESPONSE [{}][{}]", logId, requestURI);
+        long startTime = (long) request.getAttribute(START_TIME);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        log.info("RESPONSE [{}][{}][{}ms][{}]", logId, requestURI, duration, response.getStatus());
+
         if (ex != null) {
             log.error("afterCompletion error!!", ex);
         }
     }
 }
+
