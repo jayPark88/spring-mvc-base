672e06abe8619cf79ef22fcf56d34923b021620e
jayPark8282 <jaypark8282@gmail.com>
2024-07-19 23:44:50 +0900

    🔥 delete : 사용하지 않은 entity 삭제

--- core-module/src/main/java/com/jaypark8282/core/jpa/repository/CommunityBoardRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.jaypark8282.core.jpa.repository;
-//
-//
-//import com.jaypark8282.core.jpa.entity.CommunityBoardEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CommunityBoardRepository extends JpaRepository<CommunityBoardEntity, Long> {
-//}
--- core-module/src/main/java/com/jaypark8282/core/jpa/repository/ServiceNoticeBoardRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.jaypark8282.core.jpa.repository;
-//
-//import com.jaypark8282.core.jpa.entity.ServiceNoticeBoardEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ServiceNoticeBoardRepository extends JpaRepository<ServiceNoticeBoardEntity, Long> {
-//}
