9dac1766d727f6635c4b0ef496275a6da3061cd4
jayPark88 <jaypark8282@gmail.com>
2024-09-10 23:42:47 +0900

    add: 주문 생성 추가

---
+++ base-module/src/main/java/com/jaypark8282/base/api/v1/order/controller/OrderController.java
@@ -0,0 +1,50 @@
+package com.jaypark8282.base.api.v1.order.controller;
+
+import com.jaypark8282.base.api.v1.order.dto.OrderRequestDto;
+import com.jaypark8282.base.api.v1.order.service.OrderService;
+import com.jaypark8282.core.exception.CustomException;
+import com.jaypark8282.core.model.OrderModel;
+import com.jaypark8282.core.resonse.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import static com.jaypark8282.core.exception.enums.ResponseErrorCode.FAIL_404;
+import static com.jaypark8282.core.exception.enums.ResponseErrorCode.FAIL_500;
+
+@RestController
+@RequestMapping("/v1/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+    private final OrderService orderService;
+    private final MessageSource messageSource;
+
+    @PostMapping
+    public CommonResponse<OrderModel> createOrder(@Valid @RequestBody OrderRequestDto orderRequestDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomException(FAIL_404.code(),
+                    messageSource.getMessage("http.status.bad.request", null, LocaleContextHolder.getLocale()),
+                    HttpStatus.BAD_REQUEST);
+        }
+        try {
+            return new CommonResponse<>(orderService.createOrderInfo(orderRequestDto));
+        } catch (DataAccessException e) {
+            log.error("Order creation failed: {}", e.getMessage(), e);
+            throw new CustomException(FAIL_500.code(),
+                    messageSource.getMessage("order.save.fail", null, LocaleContextHolder.getLocale()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
--- base-module/src/main/java/com/jaypark8282/base/api/v1/order/service/OrderService.java
+++ base-module/src/main/java/com/jaypark8282/base/api/v1/order/service/OrderService.java
@@ -1,15 +1,39 @@
 package com.jaypark8282.base.api.v1.order.service;
 
+import com.jaypark8282.base.api.v1.order.dto.OrderRequestDto;
+import com.jaypark8282.core.jpa.repository.OrderRepository;
 import com.jaypark8282.core.model.OrderModel;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class OrderService {
 
-    //todo: order Info create method ing...
-    public OrderModel createOrderInfo(){
-        return null;
+    private final OrderRepository orderRepository;
+
+    /**
+     *
+     * @param orderRequestDto
+     * @return
+     */
+    @Transactional
+    public OrderModel createOrderInfo(OrderRequestDto orderRequestDto){
+        OrderModel orderModel = OrderModel.builder()
+                .userId(orderRequestDto.getUserId())
+                .totalAmount(orderRequestDto.getTotalAmount())
+                .status(orderRequestDto.getStatus())
+                .paymentMethod(orderRequestDto.getPaymentMethod())
+                .shippingAddress(orderRequestDto.getShippingAddress())
+                .orderDateTime(LocalDateTime.now())
+                .build();
+
+        orderRepository.save(orderModel.to());
+        return orderModel;
     }
 }
